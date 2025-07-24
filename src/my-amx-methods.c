fc5069c485bfa2a774606b5aa51b380d391e3553
Amal P Mathews <amalpmathews2003@gmail.com>
2025-07-24 15:33:55 +0530

    event when a parameter changes

--- src/my-amx-methods.c
+++ src/my-amx-methods.c
@@ -5,52 +5,62 @@
 #include <amxc/amxc.h>
 
 void _on_person_changed(const char *const event_name,
-               const amxc_var_t *const event_data,
-               void *const priv)
+                        const amxc_var_t *const event_data,
+                        void *const priv)
 {
     printf("AMXO_EVENT: Event triggered with data: \n");
 
     char *data_str = amxc_var_dyncast(cstring_t, event_data);
     // printf("Event data: %s\n", data_str ? data_str : "NULL");
 
-    amxd_dm_t* dm = my_amx_get_dm();
+    amxd_dm_t *dm = my_amx_get_dm();
     amxd_object_t *root = amxd_dm_get_root(dm);
-    amxd_object_t *person = amxd_dm_findf(dm, "%s","Person.");
-    
-    
+    amxd_object_t *person = amxd_dm_findf(dm, "%s", "Person.");
+
     amxc_var_t data;
     amxc_var_init(&data);
     amxc_var_set(cstring_t, &data, "Person data changed");
 
-    amxd_object_emit_signal(person,"my_event", &data);
+    amxd_object_emit_signal(person, "my_event", &data);
+}
 
-    amxc_var_set(cstring_t, &data, "event chain completed");
+void _on_person_age_changed(const char *const event_name,
+                            const amxc_var_t *const event_data,
+                            void *const priv)
+{
+    printf("AMXO_EVENT: Age changed event triggered with data: \n");
 
-    amxd_object_emit_signal(person,"my_event2", &data);
+    amxd_dm_t *dm = my_amx_get_dm();
+    amxd_object_t *root = amxd_dm_get_root(dm);
+    amxd_object_t *person = amxd_dm_findf(dm, "%s", "Person.");
+
+    amxc_var_t data;
+    amxc_var_init(&data);
+    amxc_var_set(cstring_t, &data, "happy birthday");
+
+    amxd_object_emit_signal(person, "my_event2", &data);
 }
 
 void _on_my_event(const char *const event_name,
-               const amxc_var_t *const event_data,
-               void *const priv)
+                  const amxc_var_t *const event_data,
+                  void *const priv)
 {
     printf("AMXO_MY_EVENT: Custom event '%s' triggered with data: \n", event_name);
 
     char *data_str = amxc_var_dyncast(cstring_t, event_data);
 
     // printf("Event data: %s\n", data_str ? data_str : "NULL");
-
 }
 
 void on_my_event2(const char *const event_name,
-               const amxc_var_t *const event_data,
-               void *const priv)
+                  const amxc_var_t *const event_data,
+                  void *const priv)
 {
     printf("AMXO_MY_EVENT2: Custom event '%s' triggered with data: \n", event_name);
 
     char *data_str = amxc_var_dyncast(cstring_t, event_data);
 
     // printf("Event data: %s\n", data_str ? data_str : "NULL");
-
 }
 
 int _on_read_name()
@@ -71,10 +81,10 @@ int _on_write()
     return 0;
 }
 
-amxd_status_t _Person_say_my_name(amxd_object_t* greeter,
-                           UNUSED amxd_function_t* func,
-                           amxc_var_t* args,
-                           amxc_var_t* ret)
+amxd_status_t _Person_say_my_name(amxd_object_t *greeter,
+                                  UNUSED amxd_function_t *func,
+                                  amxc_var_t *args,
+                                  amxc_var_t *ret)
 {
     printf("AMXO_SAY_MY_NAME: My name is MyAMX!\n");
 
