97ca2cb7a014a72d10a0756fa03641c83f2f7735
amalpmathews2003 <amalpmathews2003@gmail.com>
2025-08-01 11:48:26 +0000

    main.c working

--- src/main.c
+++ src/main.c
@@ -8,19 +8,17 @@ int main(int argc, char *argv[])
     amxrt_new();
 
     amxc_var_t* config = amxrt_get_config();
-    amxrt_config_init(argc,argv,&index,NULL);
     amxo_parser_t *parser = amxrt_get_parser();
     amxd_dm_t *dm = amxrt_get_dm();
-    amxrt_config_scan_backend_dirs();
-
     amxo_parser_parse_file(parser, ODL_FILE, amxd_dm_get_root(dm));
+
+    amxrt_config_scan_backend_dirs();
 
     amxrt_connect();
     amxrt_el_create();
     amxrt_register_or_wait();
-    printf("AMX application started\n");
+    // printf("AMX application started\n");
     amxrt_el_start();
-    printf("AMX application started\n");
 
     amxrt_stop();
     amxrt_delete();
--- src/my-amx-methods.c
+++ src/my-amx-methods.c
@@ -12,15 +12,15 @@ amxd_status_t _handle_name_write(amxd_object_t *object, UNUSED amxd_param_t *par
     return amxd_action_param_write(object, param, reason, args, retval, priv);
 }
 
-amxd_status_t _Person_say_my_name(amxd_object_t *greeter,
-                                  UNUSED amxd_function_t *func,
-                                  amxc_var_t *args,
-                                  amxc_var_t *ret)
-{
-    char *name = amxd_object_get_value(cstring_t, greeter, "name", NULL);
-    amxc_var_set(cstring_t, ret, name);
-    return amxd_status_ok;
-}
+// amxd_status_t _Person_say_my_name(amxd_object_t *greeter,
+//                                   UNUSED amxd_function_t *func,
+//                                   amxc_var_t *args,
+//                                   amxc_var_t *ret)
+// {
+//     char *name = amxd_object_get_value(cstring_t, greeter, "name", NULL);
+//     amxc_var_set(cstring_t, ret, name);
+//     return amxd_status_ok;
+// }
 
 amxd_status_t _say_my_name(amxd_object_t *greeter,
                            UNUSED amxd_function_t *func,
