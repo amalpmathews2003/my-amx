67e83978fcf092eb8966ce8bba5561845516cb5c
amalpmathews2003 <amalpmathews2003@gmail.com>
2025-08-01 05:15:01 +0000

    custom validater

--- src/my-amx-actions.c
+++ src/my-amx-actions.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "my-amx.h"
 #include "my-amx-actions.h"
+
+#include <amxd/amxd_parameter.h>
 
 amxd_status_t _read_education(UNUSED amxd_object_t *object,
                               UNUSED amxd_param_t *param,
@@ -25,7 +28,49 @@ amxd_status_t _read_education(UNUSED amxd_object_t *object,
     size_t index = rand() % (sizeof(educations) / sizeof(educations[0]));
     const char *education = educations[index];
 
-    amxc_var_set(cstring_t,retval, education);
+    amxc_var_set(cstring_t, retval, education);
 
     return status;
+}
+
+int stricmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (tolower((unsigned char)*s1) != tolower((unsigned char)*s2)) {
+            break;
+        }
+        s1++;
+        s2++;
+    }
+    return (unsigned char)*s1 - (unsigned char)*s2;
+}
+
+
+amxd_status_t _check_is_valid_organ(amxd_object_t *object, amxd_path_t *param,
+                                    amxd_action_t reason, const amxc_var_t *const args,
+                                    amxc_var_t *const retval, UNUSED void *priv)
+{
+   
+    const char* organ = amxc_var_constcast(cstring_t, args);
+    if (organ == NULL || organ[0] == '\0')
+    {
+        return amxd_status_ok;
+    }
+    printf("Validating organ: %s\n", organ);
+    const char *organs[] = {
+        "",
+        "Heart",
+        "Liver",
+        "Kidney",
+        "Lungs",
+        "Brain",
+    };
+
+    for (size_t i = 0; i < sizeof(organs) / sizeof(organs[0]); i++)
+    {
+        if (stricmp(organ, organs[i]) == 0)
+        {
+            return amxd_status_ok;
+        }
+    }
+    return amxd_status_invalid_value;
 }
